c65dabe3235f4cebff48a85609d546b339c3b378
Vinnie Vu <vinhtvu2@gmail.com>
2016-09-22 15:21:49 -0400

    Added else statements for selecting both options.

--- includes/header_common.php
+++ includes/header_common.php
@@ -9,16 +9,24 @@ if ( !class_exists( 'UCF_Header_Common' ) )
 	{
 		public function display_header()
 		{
-			$headerscript = '<script type="text/javascript" id="ucfhb-script" src="//universityheader.ucf.edu/bar/js/university-header.js';
+            $headerscript = '<script type="text/javascript" id="ucfhb-script" src="//universityheader.ucf.edu/bar/js/university-header.js';
+            $id = "ucfhb-script";
+            $src = "//universityheader/ucf.edu/bar/js/university-header.js";
+            $bootstrap_2_overrides = get_option('bootstrap_2_overrides');
+            $use_1200_breakpoint = get_option('use_1200_breakpoint');
 
-			if ( get_option( 'bootstrap_2_overrides' ) )
+            if ( $bootstrap_2_overrides && !$use_1200_breakpoint )
 			{
 				$headerscript .= '?use-bootstrap-overrides=1';
 			}
-			if ( get_option( 'use_1200_breakpoint' ) )
+			else if ( !$bootstrap_2_overrides && $use_1200_breakpoint)
 			{
 				$headerscript .= '?use-1200-breakpoint=1';
 			}
+            else if ( $bootstrap_2_overrides && $use_1200_breakpoint)
+            {
+                $headerscript .= '?use-1200-breakpoint=1&use-bootstrap-overrides=1';
+            }
 
 			$headerscript .= '"></script>';
 			echo $headerscript;
