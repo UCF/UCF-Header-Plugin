54f2aa344d9e3d96b94bdc78fbafbd2fb3b31d84
Vinnie Vu <vinhtvu2@gmail.com>
2016-09-22 15:26:16 -0400

    Only show header on non-admin pages.

--- includes/header_common.php
+++ includes/header_common.php
@@ -5,33 +5,36 @@
 
 if ( !class_exists( 'UCF_Header_Common' ) )
 {
-	class UCF_Header_Common
-	{
-		public function display_header()
-		{
-            $headerscript = '<script type="text/javascript" id="ucfhb-script" src="//universityheader.ucf.edu/bar/js/university-header.js';
-            $id = "ucfhb-script";
-            $src = "//universityheader/ucf.edu/bar/js/university-header.js";
-            $bootstrap_2_overrides = get_option('bootstrap_2_overrides');
-            $use_1200_breakpoint = get_option('use_1200_breakpoint');
-
-            if ( $bootstrap_2_overrides && !$use_1200_breakpoint )
-			{
-				$headerscript .= '?use-bootstrap-overrides=1';
-			}
-			else if ( !$bootstrap_2_overrides && $use_1200_breakpoint)
-			{
-				$headerscript .= '?use-1200-breakpoint=1';
-			}
-            else if ( $bootstrap_2_overrides && $use_1200_breakpoint)
+    class UCF_Header_Common
+    {
+        public function display_header()
+        {
+            if (!is_admin())
             {
-                $headerscript .= '?use-1200-breakpoint=1&use-bootstrap-overrides=1';
-            }
+                $headerscript = '<script type="text/javascript" id="ucfhb-script" src="//universityheader.ucf.edu/bar/js/university-header.js';
+                $id = "ucfhb-script";
+                $src = "//universityheader/ucf.edu/bar/js/university-header.js";
+                $bootstrap_2_overrides = get_option('bootstrap_2_overrides');
+                $use_1200_breakpoint = get_option('use_1200_breakpoint');
 
-			$headerscript .= '"></script>';
-			echo $headerscript;
-		}
-	}
+                if ( $bootstrap_2_overrides && !$use_1200_breakpoint )
+                {
+                    $headerscript .= '?use-bootstrap-overrides=1';
+                }
+                else if ( !$bootstrap_2_overrides && $use_1200_breakpoint)
+                {
+                    $headerscript .= '?use-1200-breakpoint=1';
+                }
+                else if ( $bootstrap_2_overrides && $use_1200_breakpoint)
+                {
+                    $headerscript .= '?use-1200-breakpoint=1&use-bootstrap-overrides=1';
+                }
+
+                $headerscript .= '"></script>';
+                echo $headerscript;
+            }
+        }
+    }
 }
 
 ?>
