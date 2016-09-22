da96a0d5aedeb297eacccbaa6fd06c72cab4f10f
Vinnie Vu <vinhtvu2@gmail.com>
2016-09-22 15:26:29 -0400

    Convert tabs to spaces.

--- ucf-header.php
+++ ucf-header.php
@@ -8,19 +8,19 @@ License: GPL3
 */
 
 if ( ! defined( 'WPINC' ) ) {
-	die;
+    die;
 }
 
 add_action( 'plugins_loaded', function()
 {
 
-	define( 'UCF_HEADER__PLUGIN_DIR', __FILE__ );
+    define( 'UCF_HEADER__PLUGIN_DIR', __FILE__ );
 
-	require_once 'includes/header_options.php';
-	require_once 'includes/header_common.php';
+    require_once 'includes/header_options.php';
+    require_once 'includes/header_common.php';
 
-	add_action( 'admin_menu', array( 'UCF_Header_Config', 'add_options_page' ) );
-	add_action( 'wp_footer', array( 'UCF_Header_Common', 'display_header') );
+    add_action( 'admin_menu', array( 'UCF_Header_Config', 'add_options_page' ) );
+    add_action( 'wp_footer', array( 'UCF_Header_Common', 'display_header') );
 
 } );
 
