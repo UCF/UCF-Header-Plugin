b67711912a71f0ac85538432bddebd2118ecc138
Vinnie Vu <vinhtvu2@gmail.com>
2016-09-27 11:35:28 -0400

    Add id to javascript.

--- includes/header_common.php
+++ includes/header_common.php
@@ -7,17 +7,16 @@ if ( !class_exists( 'UCF_Header_Common' ) )
 {
     class UCF_Header_Common
     {
-        public function display_header()
+        public static function display_header()
         {
             if (!is_admin())
             {
-                $id = "ucfhb-script";
                 $src = "//universityheader.ucf.edu/bar/js/university-header.js";
-                $params = array (
+                $params = array
+                (
                     "bootstrap_2_overrides" => get_option('bootstrap_2_overrides'),
                     "use_1200_breakpoint" => get_option('use_1200_breakpoint')
                 );
-                var_dump($params);
                 //There is at least one option checked.
                 if(count(array_filter($params)))
                 {
@@ -32,10 +31,21 @@ if ( !class_exists( 'UCF_Header_Common' ) )
                     unset($value);
                     $src = substr($src, 0, -1);
                 }
-
                 wp_register_script('ucf-header', $src, array(), null, true);
                 wp_enqueue_script('ucf-header',$src, array(), null, true);
+                add_filter('clean_url', array('UCF_Header_Common', 'add_id_to_ucfhb'), 10, 3);
             }
+        }
+
+        /**
+        * Add ID attribute to registered University Header script.
+        **/
+        public static function add_id_to_ucfhb($url) {
+            if ( (false !== strpos($url, 'bar/js/university-header.js')) || (false !== strpos($url, 'bar/js/university-header-full.js')) ) {
+            remove_filter('clean_url', 'add_id_to_ucfhb', 10, 3);
+            return "$url' id='ucfhb-script";
+            }
+            return $url;
         }
     }
 }
